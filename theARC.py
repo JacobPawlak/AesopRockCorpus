754de4b4eaa40de6c3cc2d275827fdaf8268b472
Jacob Pawlak <jcpa229@cs.uky.edu>
2018-03-06 16:39:43 -0500

    added some comments and stuff i think, I am pretty sure

--- theARC.py
+++ theARC.py
@@ -4,30 +4,56 @@
 
 import os, glob
 
-
+#list of song titles
 song_titles = []
+#list of album titles
 album_titles = []
-albums = {}
-
+#whole dictionary that will get turned into a json file
+ARC = {}
 
 def clean_files():
+
+	#change the current directory to 'Albums'
 	os.chdir("Albums")
+	#grab a list of all the subdirectories (of Albums)
 	subds = os.listdir()
 
+	#for each of the aesop rock albums (subdirectory in subdirectories)
 	for subd in subds:
+		#change the current directory to the album subdirectory
 		os.chdir(subd)
 		print("\t" + subd)
-		album_titles.append(subd)
+		#if the album title is not in the list of album titles, add it
+		if subd not in album_titles:
+			album_titles.append(subd)
+		#for each song (file will look like Blah.song)
 		for file in glob.glob("*.song"):
 			print(file)
-			song_titles.append(file[:-5])
+			#if the song is not yet in the list of songs, add it
+			if file[:-5] not in song_titles:
+				song_titles.append(file[:-5])
+		#go up a directory for the loop
 		os.chdir("..")
 
+	return
+
+def cf_helper_open(song_file):
+
+
+	return
 
 def main():
 
+	#scan in and clean the files
 	clean_files()
+	print()
 	print(song_titles)
+	print()
+	print(len(song_titles))
+	print()
 	print(album_titles)
+	print()
+	print(len(album_titles))
+	print()
 
 main()
